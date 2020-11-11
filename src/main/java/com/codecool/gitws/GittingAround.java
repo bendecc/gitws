663f5e9f8b0dfa743ab1080f451d283f5ff23ef6
Bendeguz Hajdu <bendeguz.hajdu@hawatconsulting.com>
2020-11-11 09:52:00 +0100

    sayHello method

--- src/main/java/com/codecool/gitws/GittingAround.java
+++ src/main/java/com/codecool/gitws/GittingAround.java
@@ -2,6 +2,10 @@ package com.codecool.gitws;
 
 public class GittingAround {
     public static void main(String[] args) {
+        sayHello();
+    }
+
+    public static void sayHello() {
         System.out.println("Hello Git");
     }
 }
