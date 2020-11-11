d39e013e161f0f99e979a986e8876ca620ec8810
Bendeguz Hajdu <bendeguz.hajdu@hawatconsulting.com>
2020-11-11 10:22:06 +0100

    print was created

--- src/main/java/com/codecool/gitws/GittingAround.java
+++ src/main/java/com/codecool/gitws/GittingAround.java
@@ -3,10 +3,15 @@ package com.codecool.gitws;
 public class GittingAround {
     public static void main(String[] args) {
         sayHello();
+        print();
     }
 
     public static void sayHello() {
         System.out.println("Hello Git");
         System.out.println("change");
+    }
+
+    public static void print() {
+        System.out.println("otherBranch");
     }
 }
